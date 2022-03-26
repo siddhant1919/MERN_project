70ddc0cf5cd583683d47523331cb06380d43f164
Siddhant <siddhantgangurde99@gmail.com>
2022-03-26 16:01:44 +0530

    Added middlewares

--- projbackend/app.js
+++ projbackend/app.js
@@ -4,13 +4,35 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+const authRoutes = require('./routes/authentication')
+
+
+// PORT
 const port = process.env.PORT || 8000
 
-mongoose.connect(process.env.DATABASE)
+
+// Middlewares
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+
+
+// My Routes
+app.use("/api", authRoutes)
+
+
+// DB Connection
+mongoose
+  .connect(process.env.DATABASE)
   .then(() => console.log("DB connected"))
   .catch(err => console.log("DB connection error " + err))
 
 
+// Starting a server
 app.listen(port, () => {
   console.log(`app is running at ${port}`)
 })
