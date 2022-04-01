33c814a22e71a8b8f6a0f47b21c5e7eac127e6dc
Siddhant <siddhantgangurde99@gmail.com>
2022-04-01 21:17:27 +0530

    Created route for user's purchase list

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { getUserById, getUser, updateUser } = require('../controllers/user')
+const { getUserById, getUser, updateUser, userPurchaseList } = require('../controllers/user')
 const { isSignedIn, isAuthenticated } = require('../controllers/authentication')
 
-
+// Param
 router.param('userId', getUserById)
 
 
@@ -20,6 +20,12 @@ router.get('/user/:userId', isSignedIn, isAuthenticated, getUser)
 // @desc   Route to update the info of user
 // @access PRIVATE
 router.put('/user/:userId', isSignedIn, isAuthenticated, updateUser)
+
+// @type   PUT
+// @route  /api/ordes/user/:userId
+// @desc   Route to Get Purchase list of user
+// @access PRIVATE
+router.put('/orders/user/:userId', isSignedIn, isAuthenticated, userPurchaseList)
 
 
 module.exports = router
