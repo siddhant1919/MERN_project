b31ba36805bbe87d6e667918f8cf6638161b6f99
Siddhant <siddhantgangurde99@gmail.com>
2022-04-04 22:52:24 +0530

    Fixed bug

--- projbackend/models/product.js
+++ projbackend/models/product.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-const { Schema, ObjectId } = mongoose.Schema
+const { ObjectId } = mongoose.Schema
 
-const productSchema = new Schema({
+const productSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
