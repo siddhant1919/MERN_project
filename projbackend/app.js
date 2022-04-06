96bae66818477460069bf843f0cbf8c376f8d66b
Siddhant <siddhantgangurde99@gmail.com>
2022-04-06 13:20:01 +0530

    Added order route

--- projbackend/app.js
+++ projbackend/app.js
@@ -12,6 +12,7 @@ const authRoutes = require('./routes/authentication')
 const userRoutes = require('./routes/user')
 const categoryRoutes = require('./routes/category')
 const productRoutes = require('./routes/product')
+const orderRoutes = require('./routes/order')
 
 
 // PORT
@@ -29,6 +30,7 @@ app.use("/api", authRoutes)
 app.use("/api", userRoutes)
 app.use("/api", categoryRoutes)
 app.use("/api", productRoutes)
+app.use("/api", orderRoutes)
 
 
 // DB Connection
